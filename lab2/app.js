d5d0de062454dd950415d5df9c603df5c4c3bf41
Maksim Beliavsky <maksimbell503@gmail.com>
2023-04-24 01:06:01 +0300

    finally))))

--- lab2/app.js
+++ lab2/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fileUpload from 'express-fileupload'
 import path from 'path'
+import bodyParser from 'body-parser'
 import fs from 'fs'
 
 const app = express();
@@ -9,10 +10,12 @@ const port = 3000;
 
 app.use(express.static('public'));
 const filePath = 'data.json'
-
+app.use(bodyParser.json());
 app.use(express.urlencoded({
     extended: true
 }))
+
+app.use(express.json())
 
 app.use(fileUpload({
     createParentPath: true
@@ -20,18 +23,62 @@ app.use(fileUpload({
 
 app.listen(port, () => console.log(`Listening on ${port}`));
 
-app.get("/board", (req, res) => {
-    try{
-      res.status(200).sendFile(path.resolve(path.dirname("."), 'views', 'pages', 'board.html'))
-    }catch(e){
-      console.log(e)
-      res.status(400).json({message: 'Get error'})
+app.get("/board/", (req, res) => {
+    try {
+        res.status(200).sendFile(path.resolve(path.dirname("."), 'views', 'pages', 'board.html'))
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({
+            message: 'Get error'
+        })
     }
 })
 
-app.get('/board/get', (req, res) => {
-    const data = fs.readFileSync(filePath, 'utf8')
-    const board = JSON.parse(data)
+app.get('/board/columns', (req, res) => {
+    const board = data
     res.send(board)
 })
 
+app.post('/board/card', (req, res) => {
+    const id = +req.body.id
+    //e.target
+    const newCard = {
+        id: data.columns[id].cards.length,
+        content: req.body.content,
+    }
+
+    data.columns[id].cards.push(newCard)
+})
+
+app.post('/board/column', (req, res) => {
+    const newColumn = {
+        ...req.body
+    }
+    data.columns.push(newColumn)
+})
+
+const data = {
+    name: "First board",
+    columns: [{
+            id: 0,
+            name: "First column",
+            cards: [{
+                    id: 0,
+                    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                },
+                {
+                    id: 1,
+                    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit orem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing elit"
+                }
+            ]
+        },
+        {
+            id: 1,
+            name: "Second column",
+            cards: [{
+                id: 0,
+                content: "Lorem ipsum fdsghgffdghfgddolor sit amsfddsfdsffsfsfdsfdsfdsfdsfdset, consectetur adipiscing elit."
+            }]
+        }
+    ]
+}
--- lab2/public/scripts/script.js
+++ lab2/public/scripts/script.js
@@ -2,7 +2,7 @@ const columnSelector = document.querySelector('.columns')
 
 async function getBoard() {
 
-    const res = await fetch('board/get', {
+    const res = await fetch('board/columns', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
@@ -15,26 +15,28 @@ async function getBoard() {
             for (let col of board.columns) {
                 columnSelector.appendChild(newColumn(col))
             }
-            
-            columnSelector.innerHTML += 
-            `<div class="column">
+
+            columnSelector.innerHTML +=
+                `<div class="column">
                 <div class="clickable-card">
-                    <form action="/board/add-column" method="POST">
+                    <form class="newColumnForm">
                         <textarea class="card card__content input__add-card" type="text" name="columnName"
                             placeholder="Enter column name"></textarea>
                         <input class="button__add-card" value="Add column" type="submit" />
                     </form>
                 </div>
             </div>`
-        })
 
+            addListeners()
+        })
     }
 }
 
-function newColumn(data){
+function newColumn(data) {
     const container = document.createElement('div')
     container.classList.add('column')
     container.classList.add('university-column')
+    container.id = `col${data.id}`
 
     const columnHeading = document.createElement('h3')
     columnHeading.textContent = data.name
@@ -50,31 +52,89 @@ function newColumn(data){
         container.appendChild(newCard(card))
     }
 
-    container.innerHTML += 
-    `<div class="clickable-card">
-        <form action="/board/add-card/${data.id}" method="POST" enctype="multipart/form-data">
-            <textarea class="card card__content input__add-card" type="text" name="content"
-                placeholder="Enter heading"></textarea>
-            <input class="button__add-card" value="Add card" type="submit" />
-            <label class="input-file">
-                <input type="file" name="uploaded" id="file_input_1">
-                <span id="file_span_1">Choose file</span>
-            </label>
-        </form>
-    </div>`
+    const formDiv = document.createElement('div')
+    formDiv.classList.add('clickable-card')
 
-    console.log(container)
+    const form = document.createElement('form')
+    form.classList.add('cardForm')
+
+    form.id = 'form' + data.id
+    form.innerHTML +=
+        `<textarea class="card card__content input__add-card" type="text" name="content"
+            placeholder="Enter heading"></textarea>
+        <input id="${data.id}" class="button__add-card" value="Add card" type="submit"/>
+        <label class="input-file">
+            <input type="file" name="uploaded" id="file_input_1">
+            <span id="file_span_1">Choose file</span>
+        </label>`
+
+    formDiv.appendChild(form)
+    container.appendChild(formDiv)
 
     return container
 }
 
-function newCard(card){
+async function addCard(e) {
+    const id = e.target.id.slice(4, e.target.id.length)
+    const form = document.querySelector(`#form${id}`)
+
+    const currentColumn = document.querySelector(`#col${id}`)
+    console.log(currentColumn)
+
+    const card = {
+        id,
+        content: form.content.value,
+    }
+
+    form.reset();
+    currentColumn.insertBefore(newCard(card), currentColumn.children[currentColumn.children.length - 1])
+
+    const res = await fetch("board/card", {
+        method: "POST",
+        // x-www-form-urlencoded
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(card)
+    });
+
+    if (res.ok === true) {
+        const user = await response.json();
+    }
+}
+
+async function addColumn(e) {
+
+    const column = {
+        id: columnSelector.children.length - 1,
+        name: e.target.columnName.value,
+        cards: [],
+    }
+    console.log(column)
+
+    e.target.reset();
+    columnSelector.insertBefore(newColumn(column), columnSelector.children[columnSelector.children.length - 1])
+
+    const res = await fetch("board/column", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(column)
+    });
+
+    if (res.ok === true) {
+        const user = await response.json();
+    }
+}
+
+function newCard(card) {
     const container = document.createElement('div')
     container.classList.add('clickable-card')
     container.classList.add('card')
 
     container.innerHTML += `<a class="clickable-card__link" href="/board/open-card/${card.id}"></a>`
-    if(card.file)
+    if (card.file)
         container.innerHTML += `<a class="card__content download-file" href="/files/${card.file}" download="">Attached file</a>`
 
     const name = document.createElement('p')
@@ -83,6 +143,27 @@ function newCard(card){
     container.appendChild(name)
 
     return container
+}
+
+function addListeners() {
+    const forms = document.querySelectorAll('.cardForm')
+    console.log(forms)
+
+    for (let form of forms) {
+        console.log(form)
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            console.log('neq card submitted')
+            addCard(e)
+        })
+    }
+
+    const newColumnForm = document.querySelector('.newColumnForm')
+    newColumnForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log('new column submitted')
+        addColumn(e)
+    })
 }
 
 getBoard()
